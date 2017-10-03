eb2c93ac21586ee5dbd502a4062a00a2edd680f3
renatogalvones <galvon@gmail.com>
2017-10-03 20:08:19 -0300

    Input coach working

--- src/components/Race.js
+++ src/components/Race.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addCoach } from '../actions/';
 
-const Race = ({ coaches, onAddCoach }) => {
-  return (
-    <div>
-      <h1>Corrida dos campeões</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Treinador</th>
-            <th>Pontos</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr></tr>
-        </tbody>
-      </table>
-      <button onClick={() => { onAddCoach('galvao'); }}>ADD</button>
-      {/* <button onClick={() => { store.dispatch({ type: 'DECREMENT' })} }>REMOVE</button> */}
-    </div>
-  );
-};
+class Race extends Component {
+  constructor() {
+    super();
+
+    this.addCoach = this.addCoach.bind(this);
+  }
+
+  addCoach(e) {
+    e.preventDefault();
+    const { onAddCoach } = this.props;
+    const newCoach = this.newCoach.value;
+    this.newCoach.value = '';
+    onAddCoach(newCoach);
+  }
+
+  render() {
+    const { coaches } = this.props;
+    const coachesList = [];
+    coaches.forEach(coach => coachesList.push(<tr key={coach.id}><td>{coach.name}</td></tr>));
+
+    return (
+      <div>
+        {/* <h1>Corrida dos campeões</h1> */}
+        <form onSubmit={this.addCoach}>
+          <p>Adicionar Treinador</p>
+          <input type="text" ref={(ref) => { this.newCoach = ref; }} />
+        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>Treinador</th>
+              <th>Pontos</th>
+            </tr>
+          </thead>
+          <tbody>
+            {coachesList}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   coaches: state.coaches,
@@ -33,7 +55,7 @@ const mapDispatchToProps = dispatch => ({
 
 const RaceConnected = connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(Race);
 
 export default RaceConnected;
